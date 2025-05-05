f9b3ee784765d937f207d57283d6fcae35642d62
ALADETAN-IFE <famousfotune123@gmail.com>
2025-05-05 03:17:36 +0100

    second commit

---
+++ src/context/ApolloProvider.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider as Provider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: import.meta.env.VITE_SERVER_URL,
+  cache: new InMemoryCache(),
+});
+
+const ApolloProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <Provider client={client}>
+      {children}
+    </Provider>
+  );
+};
+
+export default ApolloProvider;
